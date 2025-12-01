7c9f9601f480450e77adedc7dff4c3b9132f2cb5
Jose Pablo Rojas Cavallini <kvallini@gmail.com>
2025-11-30 21:57:58 -0600

    sweet alert

--- Site.Master.designer.vb
+++ Site.Master.designer.vb
@@ -32,6 +32,15 @@ Partial Public Class Site
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
     '''<summary>
+    '''Control ScriptManager1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ScriptManager1 As Global.System.Web.UI.ScriptManager
+
+    '''<summary>
     '''Control MainContent.
     '''</summary>
     '''<remarks>
